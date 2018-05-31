30ffd5a0ef87d676a7bd48f7298fb8a4fb0e998c
Sam Cole <samcole@windowslive.com>
2018-05-31 18:23:23 +1200

    Part 1 upload

--- src/assignment3/core.clj
+++ src/assignment3/core.clj
@@ -9,7 +9,7 @@
   "Function to create a new image."
   [width height]
   (BufferedImage. width height BufferedImage/TYPE_INT_RGB)
-)
+  )
 
 (defn read-image
   "Function to read an image from a file."
@@ -37,8 +37,7 @@
   "Function to get the height of an image."
   [image]
   (.getHeight image)
-)
-
+  )
 
 (defn get-rgb
   "Function to get the RGB components of a pixel in a vector of length 3."
@@ -60,7 +59,30 @@
                   (bit-shift-left green 0) ) ]
        (.setRGB image x y rgb)
    )
-)
+  )
+
+(defn get-grey
+  "Function to get the grey vaule of pixel"
+  [image x y]
+  (let [result (get-rgb image x y)]
+    (/ (reduce + result) 3)
+    )
+  )
+
+(defn get-grey-in-bounds
+  "Function to get the grey value or the hotspot if it is not in bounds."
+  [image x y hotspot]
+  (let [width (get-width image)
+        height (get-height image)]
+    (if (< x 0)
+      hotspot
+      (if (>= x width)
+        hotspot
+        (if (< y 0)
+          hotspot
+          (if (>= y height)
+            hotspot
+            (get-grey image x y)))))))
 
 (defn set-grey
   "Function to set the grey value of a pixel."
@@ -71,42 +93,7 @@
 (defn -main
   "Test the image functions."
   [& args]
-  (let [filename-in  "/home/sam/assignment3/resources/car1.jpg"
-        filename-out "/home/sam/assignment3/resources/car1-mod.png"
-        test-image   (read-image filename-in)
-        image-width  (get-width test-image)
-        image-height (get-height test-image)
-        cropped-image (new-image (/ image-width 2)
-                                   image-height)
-        ]
-    ; report that an image has been read
-    (println "image read is" filename-in
-             "with dimensions" image-width "X" image-height)
-    ; convert the image to greyscale
-    (dotimes [x image-width]
-      (dotimes [y image-height]
-        (let [rgb  (get-rgb test-image x y)
-              grey (int (/ (reduce + rgb) 3.0))]
-          (set-grey test-image x y grey)
-        )
-      )
-    )
-    ; crop half the image into a copy
-    (dotimes [x (get-width cropped-image)]
-      (dotimes [y (get-height cropped-image)]
-        (set-rgb cropped-image
-                  x y
-                  (get-rgb test-image x y))
-        )
-    )
-    ; save the modified test image
-    (save-image cropped-image "png" filename-out)
-                                        ; report
-    
-    (println "image written is" filename-out
-             "with dimensions" (get-width cropped-image)
-             "X" (get-height cropped-image))
-  )
+  
 )
 
 (defn  create-matrix
@@ -118,7 +105,7 @@
    )
   )
 
-(defn  get-dims-matrix
+(defn get-dims-matrix
   [matrix]
   {:nrows (count  matrix)
    :ncols (count (first  matrix ))}
@@ -147,13 +134,11 @@
     (println)
     ))
 
-
 (defn  get-matrix-row
   "Function  to get a matrix  row."
   [matrix  row]
   (nth  matrix  row)
   )
-
 
 (defn  get-matrix-col
   "Function  to get a matrix  column."
@@ -161,44 +146,92 @@
   (vec (map #(nth % col) matrix ))
   )
 
-;; (defn  query-matrix
-
-;;   "Function  to find  the  first  occurrence  of an item in the  matrix  and  return  its  indices , or nil if it does  not  exist."
-;;   [matrix  query-val]
-;;   (let [index   ( .indexOf (flatten  matrix) query-val)
-;;         ncols (: ncols (get-dims-matrix  matrix ))]
-;;     (when (>= index  0)
-;;       [(int (/  index  ncols))
-;;        (mod  index  ncols )]
-;;       ))
-;; )
-
-(create-matrix 2 3)
- 
-
-(defn krish
+(defn kirsch
   "Ables krish filter in direction i from given file"
-  [file i]
-
-  
+  [filename i]
+  (let [image (read-image filename)
+        width (get-width image)
+        height (get-height image)
+        new-filtered-bad-boi (new-image width height )]
+    (dotimes [x width]
+      (dotimes [y height]
+        (set-grey new-filtered-bad-boi
+                  x y
+                  (filter (get-neighbours x y image)
+                          (filters i)))))
+    (save-image new-filtered-bad-boi "png" (str filename "superBABBOI"))
+    )
   )
+
+(kirsch "/home/sam/assignment3/resources/car1.jpg" 0)
+  
+
 
 (defn get-neighbours
   "Takes a hotspot and gets the pixels around it"
   [x y file]
   (let [top1-coord [(- x 1)(- y 1)]
-        top2-coord [(x)(- y 1)]
+        top2-coord [ x (- y 1)]
         top3-coord [(+ x 1)(- y 1)]
-        mid1-coord [(x-1)(y)]
-        hotspot [(x)(y)]
-        mid3-coord [(+ x 1)(y)]
+        mid1-coord [(- x 1) y ]
+        hotspot (get-grey file x y)
+        mid3-coord [(+ x 1) y]
         bot1-coord [(- x 1)(+ y 1)]
-        bot2-coord [(x)(+ y 1)]
+        bot2-coord [ x (+ y 1)]
         bot3-coord [(+ x 1)(+ y 1)]
-        initial-matrix(create-matrix 3 3)
+        initial-matrix (create-matrix 3 3)
+        modified-matrix (-> (set-matrix initial-matrix 0 0 (get-grey-in-bounds file (first top1-coord) (last top1-coord) hotspot))
+                            (set-matrix 0 1 (get-grey-in-bounds file (first top2-coord) (last top2-coord) hotspot))
+                            (set-matrix 0 2 (get-grey-in-bounds file (first top3-coord) (last top3-coord) hotspot))
+                            (set-matrix 1 0 (get-grey-in-bounds file (first mid1-coord) (last mid1-coord) hotspot))
+                            (set-matrix 1 1 hotspot)
+                            (set-matrix 1 2 (get-grey-in-bounds file (first mid3-coord) (last mid3-coord) hotspot))
+                            (set-matrix 2 0 (get-grey-in-bounds file (first bot1-coord) (last bot1-coord) hotspot))
+                            (set-matrix 2 1 (get-grey-in-bounds file (first bot2-coord) (last bot2-coord) hotspot))
+                            (set-matrix 2 2 (get-grey-in-bounds file (first bot3-coord) (last bot3-coord) hotspot))
+                            )
         ]
-    ()
-
+    modified-matrix
     )
-  
   )
+;; (get-neighbours 2 3 (read-image "/home/sam/assignment3/resources/car1.jpg"))
+;; (get-neighbours 0 0 (read-image "/home/sam/assignment3/resources/car1.jpg"))
+
+(defn multiply-matrix
+  "Takes two matrices and times them together."
+  [matrix1 matrix2]
+  (mapv #(mapv * %1 %2) matrix1 matrix2)
+  )
+;; (multiply-matrix [[1 1 1][2 2 2]] [[3 3 3][4 4 4]])
+
+(defn sum-vector
+  "Takes a vector and returns it sum"
+  [vector]
+  (reduce + vector))
+;; (sum-vector [1 2 3])
+
+(defn sum-matrix
+  "Takes a matrix and returns the sum"
+  [matrix]
+  (reduce + (flatten matrix)))
+;; (sum-matrix [[1 1 1][2 2 2][3 3 3]])
+
+(defn filter
+  "Takes a filter and a pixel a matrix and applies the filter."
+  [filter matrix]
+  (+ 127 (Math/round (float (/ (sum-matrix (multiply-matrix filter matrix)) 8)))))
+;; (filter (filters 0) (get-neighbours 0 0 (read-image "/home/sam/assignment3/resources/car1.jpg")))
+
+(defn filters
+  "Returns 1 of 9 filters depending on the direction you give it."
+  [direction]
+  (let [kirsch-filter [[[-1 0 1][-2 0 2][-1 0 1]]
+                       [[-2 -1 0][-1 0 1][0 1 2]]
+                       [[-1 -2 -1][0 0 0][1 2 1]]
+                       [[0 -1 -2][1 0 -1][2 1 0]]
+                       [[1 0 -1][2 0 -2][1 0 -1]]
+                       [[2  1 0][1 0 -1][0 -1 -2]]
+                       [[1 2 1][0 0 0][-1 -2 -1]]                                                                                                                                                                                                                     [[][][]]
+                       [[0 1 2][-1 0 1][-2 -1 0]]]]
+    (nth kirsch-filter direction)))
+;; (filters 2)
